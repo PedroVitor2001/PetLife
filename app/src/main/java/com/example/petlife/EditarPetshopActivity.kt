7301ded449dd7d0432e618567790ee18eebd4a6d
Pedro Petrilli <ppetrilli96@gmail.com>
2024-10-20 16:29:06 -0300

    feat: petshot edit acitivity

---
+++ app/src/main/java/com/example/petlife/EditarPetshopActivity.kt
@@ -0,0 +1,31 @@
+package com.example.petlife
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.petlife.databinding.ActivityEditarPetshopBinding
+
+class EditarPetshopActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEditarPetshopBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityEditarPetshopBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Recuperar os dados enviados pela MainActivity
+        val petshop = intent.getStringExtra("petshop")
+
+        // Preencher o campo com o dado existente
+        binding.inputPetshop.setText(petshop)
+
+        // Botão para salvar os dados
+        binding.btnSalvarPetshop.setOnClickListener {
+            val resultIntent = Intent()
+            resultIntent.putExtra("petshop", binding.inputPetshop.text.toString())
+            setResult(RESULT_OK, resultIntent)
+            finish()
+        }
+    }
+}
